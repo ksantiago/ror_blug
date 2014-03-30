4277a931c34f78d4ee6f510ae84519aefa97efd8
Katherine Santiago <santiago.kat@gmail.com>
2014-03-30 12:17:59 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorBlog::Application.config.secret_token = 'a7be81815ff690b9eaa9b4345a4638625391cad76ccffb8ad5f2eaa717858d645939af22c7130ff6c1d01e6083bf426597c45488ae64a3f315ddcc8955d1d016'
